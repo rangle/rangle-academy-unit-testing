07d337e3eead1fc2a6e5cc4be3c94acbe4650bad
Thomas Marek <ttmarek@outlook.com>
2016-11-01 13:55:41 -0400

    Replace wage-calculator with pay-calculator (#7)

---
+++ exercises/01-thinking-like-a-tester/pay-calculator.js
@@ -0,0 +1,12 @@
+/**
+ * Tasks:
+ *
+ * Step 1.
+ * - Write calculatePay such that it passes the first test
+ * - Uncomment the second test and rewrite calculatePay to make both tests pass
+ * - Uncomment each subsequent test one by one while updating calculatePay until
+ *   you're left with a function that passes every unit test
+ */
+
+export function calculatePay() {
+}
