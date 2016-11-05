1b112d3155e33b6882f9987710cd461a9a064251
Thomas Marek <ttmarek@outlook.com>
2016-11-05 14:57:19 -0400

    Refactor exercise 3

--- exercises/03-saving-time-bug/run.js
+++ exercises/03-saving-time-bug/run.js
@@ -1,5 +1,31 @@
 require('babel-register');
 
-const app = require('./app');
+const { deviceHealth } = require('./app');
 
-app.run();
+const runRule = () => {
+  let cpuUsage = Math.floor(Math.random() * 100, 2);
+  let result  = deviceHealth(cpuUsage);
+
+  console.log('Device Health is', result);
+};
+
+const run = () => {
+  const MAX_RUN = 25;
+  let currentRun = 0;
+
+  const runner = setInterval(() => {
+    try {
+      runRule();
+    } catch (e) {
+      console.log('Error!');
+    }
+
+    currentRun++;
+
+    if (currentRun >= MAX_RUN) {
+      clearInterval(runner);
+    }
+  }, 1);
+};
+
+run();
