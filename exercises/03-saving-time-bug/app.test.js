04912cbe2a776fb8e55580c11ef945d6e451405f
Thomas Marek <ttmarek@outlook.com>
2016-11-18 09:31:46 -0500

    Fix unit tests so they pass (#10)

--- exercises/03-saving-time-bug/app.test.js
+++ exercises/03-saving-time-bug/app.test.js
@@ -3,29 +3,23 @@ import { expect } from 'chai';
 import { deviceHealth } from './app';
 
 describe('deviceHealth', () => {
-  const config = {
-    GOOD: { min: 0, max: 30 },
-    WARN: { min: 31, max: 60 },
-    CRITICAL: { min: 61, max: 100 },
-  };
-
   it('should return GOOD if the device is in a good state', () => {
     const expected = 'GOOD';
-    const actual = deviceHealth(config, 29).level;
+    const actual = deviceHealth(29).level;
 
     expect(actual).to.equal(expected);
   });
 
   it('should return WARN if the device is in a warning state', () => {
     const expected = 'WARN';
-    const actual = deviceHealth(config, 32).level;
+    const actual = deviceHealth(32).level;
 
     expect(actual).to.equal(expected);
   });
 
   it('should return CRITICAL if the device is in a critical state', () => {
     const expected = 'CRITICAL';
-    const actual = deviceHealth(config, 62).level;
+    const actual = deviceHealth(62).level;
 
     expect(actual).to.equal(expected);
   });
